b2d8bc703221e585f3e195802e7af95e1b1037cb
Ahmed Abulwafa Ahmed <a.abulwafa@rheagroup.com>
2021-02-04 14:39:53 +0100

    added the basic setup for the adapter, connecting to a hub data source and solution settings and license files; fixes #1

---
+++ DEHCATIA/App.xaml.cs
@@ -0,0 +1,109 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="App.xaml.cs" company="RHEA System S.A.">
+//    Copyright (c) 2021 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, Ahmed Abulwafa Ahmed
+//
+//    This file is part of DEHCATIA
+//
+//    The DEHCATIA is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    The DEHCATIA is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with this program; if not, write to the Free Software Foundation,
+//    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+using DEHCATIA.ViewModels;
+using DEHCATIA.ViewModels.Interfaces;
+using DEHCATIA.Views;
+
+namespace DEHCATIA
+{
+    using System.Reflection;
+    using System.Windows;
+
+    using Autofac;
+
+    using DEHPCommon;
+    using DEHPCommon.MappingEngine;
+    using DEHPCommon.Services.NavigationService;
+
+    using DevExpress.Xpf.Core;
+
+    using DXSplashScreenViewModel = DevExpress.Mvvm.DXSplashScreenViewModel;
+    using SplashScreen = DEHPCommon.UserInterfaces.Views.SplashScreen;
+
+    /// <summary>
+    /// Interaction logic for App.xaml
+    /// </summary>
+    public partial class App
+    {
+        /// <summary>
+        /// Initializes a new <see cref="App"/>
+        /// </summary>
+        /// <param name="containerBuilder">An optional <see cref="Container"/></param>
+        public App(ContainerBuilder containerBuilder = null)
+        {
+            this.Exit += this.OnExit;
+
+            var splashScreenViewModel = new DXSplashScreenViewModel() { Title = "DEH-CATIA Adapter" };
+            SplashScreenManager.Create(() => new SplashScreen(), splashScreenViewModel).ShowOnStartup();
+
+            containerBuilder ??= new ContainerBuilder();
+            RegisterTypes(containerBuilder);
+            RegisterViewModels(containerBuilder);
+            AppContainer.BuildContainer(containerBuilder);
+        }
+
+        /// <summary>
+        /// Occurs when the app closes, it makes sure any opc connection are properly closed
+        /// </summary>
+        /// <param name="sender">The <see cref="object"/> sender</param>
+        /// <param name="e">The <see cref="ExitEventArgs"/></param>
+        private void OnExit(object sender, ExitEventArgs e)
+        {
+        }
+
+        /// <summary>
+        /// Occurs when <see cref="Application"/> starts, starts a new <see cref="ILifetimeScope"/> and open the <see cref="Application.MainWindow"/>
+        /// </summary>
+        /// <param name="e">The <see cref="StartupEventArgs"/></param>
+        protected override void OnStartup(StartupEventArgs e)
+        {
+            using (var scope = AppContainer.Container.BeginLifetimeScope())
+            {
+                scope.Resolve<INavigationService>().Show<MainWindow>();
+            }
+
+            base.OnStartup(e);
+        }
+
+        /// <summary>
+        /// Registers the types that can be resolved by the <see cref="IContainer"/>
+        /// </summary>
+        /// <param name="containerBuilder">The <see cref="ContainerBuilder"/></param>
+        private static void RegisterTypes(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<MappingEngine>().As<IMappingEngine>().WithParameter(MappingEngine.ParameterName, Assembly.GetExecutingAssembly());
+        }
+
+        /// <summary>
+        /// Registers all the view model so the depencies can be injected
+        /// </summary>
+        /// <param name="containerBuilder">The <see cref="ContainerBuilder"/></param>
+        private static void RegisterViewModels(ContainerBuilder containerBuilder)
+        {
+            containerBuilder.RegisterType<MainWindowViewModel>().As<IMainWindowViewModel>().SingleInstance();
+            containerBuilder.RegisterType<HubDataSourceViewModel>().As<IHubDataSourceViewModel>();
+        }
+    }
+}
---
+++ DEHCATIA/ViewModels/HubDataSourceViewModel.cs
@@ -0,0 +1,97 @@
+﻿// --------------------------------------------------------------------------------------------------------------------
+// <copyright file="HubDataSourceViewModel.cs" company="RHEA System S.A.">
+//    Copyright (c) 2021 RHEA System S.A.
+//
+//    Author: Sam Gerené, Alex Vorobiev, Alexander van Delft, Nathanael Smiechowski, Ahmed Abulwafa Ahmed
+//
+//    This file is part of DEHCATIA
+//
+//    The DEHCATIA is free software; you can redistribute it and/or
+//    modify it under the terms of the GNU Lesser General Public
+//    License as published by the Free Software Foundation; either
+//    version 3 of the License, or (at your option) any later version.
+//
+//    The DEHCATIA is distributed in the hope that it will be useful,
+//    but WITHOUT ANY WARRANTY; without even the implied warranty of
+//    MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the GNU
+//    Lesser General Public License for more details.
+//
+//    You should have received a copy of the GNU Lesser General Public License
+//    along with this program; if not, write to the Free Software Foundation,
+//    Inc., 51 Franklin Street, Fifth Floor, Boston, MA  02110-1301, USA.
+// </copyright>
+// --------------------------------------------------------------------------------------------------------------------
+
+namespace DEHCATIA.ViewModels
+{
+    using DEHPCommon.HubController.Interfaces;
+    using DEHPCommon.Services.NavigationService;
+    using DEHPCommon.UserInterfaces.ViewModels.Interfaces;
+    using DEHPCommon.UserInterfaces.ViewModels.PublicationBrowser;
+    using DEHPCommon.UserInterfaces.Views;
+
+    using DEHCATIA.ViewModels.Interfaces;
+
+    /// <summary>
+    /// View model that represents a data source panel which holds a tree like browser, a informational header and
+    /// some control regarding the connection to the data source
+    /// </summary>
+    public sealed class HubDataSourceViewModel : DataSourceViewModel, IHubDataSourceViewModel
+    {
+        /// <summary>
+        /// The <see cref="IHubController"/>
+        /// </summary>
+        private readonly IHubController hubController;
+
+        /// <summary>
+        /// The <see cref="IObjectBrowserViewModel"/>
+        /// </summary>
+        public IObjectBrowserViewModel ObjectBrowser { get; set; }
+
+        /// <summary>
+        /// The <see cref="IPublicationBrowserViewModel"/>
+        /// </summary>
+        public IPublicationBrowserViewModel PublicationBrowser { get; set; }
+
+        /// <summary>
+        /// The <see cref="IHubBrowserHeaderViewModel"/>
+        /// </summary>
+        public IHubBrowserHeaderViewModel HubBrowserHeader { get; set; }
+
+        /// <summary>
+        /// Initializes a new <see cref="HubDataSourceViewModel"/>
+        /// </summary>
+        /// <param name="navigationService">The <see cref="INavigationService"/></param>
+        /// <param name="hubController">The <see cref="IHubController"/></param>
+        /// <param name="objectBrowser">The <see cref="IObjectBrowserViewModel"/></param>
+        /// <param name="publicationBrowser">The <see cref="IPublicationBrowserViewModel"/></param>
+        /// <param name="hubBrowserHeader">The <see cref="IHubBrowserHeaderViewModel"/></param>
+        public HubDataSourceViewModel(INavigationService navigationService, IHubController hubController, IObjectBrowserViewModel objectBrowser, IPublicationBrowserViewModel publicationBrowser, IHubBrowserHeaderViewModel hubBrowserHeader) : base(navigationService)
+        {
+            this.hubController = hubController;
+            this.ObjectBrowser = objectBrowser;
+            this.PublicationBrowser = publicationBrowser;
+            this.HubBrowserHeader = hubBrowserHeader;
+
+            this.InitializeCommands();
+        }
+
+        /// <summary>
+        /// Executes the <see cref="DataSourceViewModel.ConnectCommand"/>
+        /// </summary>
+        protected override void ConnectCommandExecute()
+        {
+            if (this.hubController.IsSessionOpen)
+            {
+                this.ObjectBrowser.Things.Clear();
+                this.hubController.Close();
+            }
+            else
+            {
+                this.NavigationService.ShowDialog<Login>();
+            }
+
+            this.UpdateConnectButtonText(this.hubController.IsSessionOpen);
+        }
+    }
+}
