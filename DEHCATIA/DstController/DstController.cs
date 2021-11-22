35d49021a84adfb8bc7a4f33a88abc70e2c4af1b
Smiechowski Nathanael <n.smiechowski@rheagroup.com>
2021-11-22 16:51:45 +0100

    Feat/gh39 save load mapping; fixes #39 (#50)
    * Bug fix Minor visual improvements and versions number bumped
    
    * Added Mapping configuration service; fixes #39

--- DEHCATIA/DstController/DstController.cs
+++ DEHCATIA/DstController/DstController.cs
@@ -30,7 +30,6 @@ namespace DEHCATIA.DstController
     using System.Reactive.Linq;
     using System.Threading;
     using System.Threading.Tasks;
-    using System.Windows;
 
     using CDP4Common;
     using CDP4Common.CommonData;
